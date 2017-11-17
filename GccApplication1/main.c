818dad8f8c54b74208d85c35633cccb33c1b5ab5
kristofersomlin <kristofer.somlin@gmail.com>
2017-11-17 12:09:38 +0100

    added arrays instead of functions

--- GccApplication1/main.c
+++ GccApplication1/main.c
@@ -4,8 +4,60 @@
 
 // Fundering, ändra alla coil funktioner till en array som PORTC får värde ifrån.
 
-int forward=1;
-void (*coil)();
+int stepper_motor[2][8] = {
+	{0b00000001,0b00000011,0b00000010,0b00000110,0b00000100,0b00001100,0b00001000,0b00001001} ,
+	{0b00000001,0b00000011,0b00000010,0b00000110,0b00000100,0b00001100,0b00001000,0b00001001}
+};
+
+
+
+int main(void)
+{
+	DDRC |= 0xFF;
+	DDRB = 0x00;
+	PORTB = 0x01;
+	// Variable to set movement of the stepper motor.
+	int forward=1;
+	int stepper_movement=0;
+	while (1)
+	{
+		if((PINB & (1<<PB0)) == 0) {
+			forward=0;
+			} else {
+			forward=1;
+		}
+
+		if(forward) {
+			if(stepper_movement<7){
+				stepper_movement++;
+			} else {
+				stepper_movement=0;
+			}
+		} else {
+			if(stepper_movement>0) {
+				stepper_movement--;
+			} else {
+				stepper_movement=7;
+			}
+		}
+
+		PORTC = stepper_motor[1][stepper_movement];
+		_delay_us(700);
+
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+/*void (*coil)();
 void coil1();
 void coil12();
 void coil2();
@@ -48,25 +100,4 @@ void coil4() {
 void coil41() {
 	PORTC = 0b00001001;
 	coil = forward ? coil1 : coil4;
-}
-
-
-int main(void)
-{
-	DDRC |= 0xFF;
-	DDRB = 0x00;
-	PORTB = 0x01;
-	coil=coil1;
-	while (1)
-	{
-		if((PINB & (1<<PB0)) == 0) {
-			forward=0;
-			} else {
-			forward=1;
-		}
-		coil();
-		_delay_us(700);
-
-
-	}
-}
+}*/
