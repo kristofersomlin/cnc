d56ab2e694184aade8be6a233f209bf40ae3dcec
kristofersomlin <kristofer.somlin@gmail.com>
2017-11-20 09:40:46 +0100

    added pointer for PORT-value to stepper struct

--- GccApplication1/main.c
+++ GccApplication1/main.c
@@ -2,19 +2,15 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-
-
-// Fundering, ändra alla coil funktioner till en array som PORTC får värde ifrån.
-
 struct stepper_motor {
-	int stepper_values[8];
+	uint8_t stepper_values[8];
 	int forward;
 	int stepper_movement;
+	volatile uint8_t *port;
 } stepper_motors[2] = {
-	{{0b00000001,0b00000011,0b00000010,0b00000110,0b00000100,0b00001100,0b00001000,0b00001001},1,0},
-	{{0b00000001,0b00000011,0b00000010,0b00000110,0b00000100,0b00001100,0b00001000,0b00001001},1,0}
+	{{0b00000001,0b00000011,0b00000010,0b00000110,0b00000100,0b00001100,0b00001000,0b00001001},1,0,&PORTC},
+	{{0b00000001,0b00000011,0b00000010,0b00000110,0b00000100,0b00001100,0b00001000,0b00001001},1,0,&PORTB}
 };
-
 
 // Ändra denna funktion till en int som returnerar 1 om den funkar och 0 om det blir fel?!?!?!?!?
 void stepper_move(int stepper) {
@@ -31,12 +27,8 @@ void stepper_move(int stepper) {
 			stepper_motors[stepper].stepper_movement=7;
 		}
 	}
-	if(stepper==0){
-		PORTC = stepper_motors[stepper].stepper_values[stepper_motors[stepper].stepper_movement];
-	}
-	else if (stepper==1) {
-		PORTB = stepper_motors[stepper].stepper_values[stepper_motors[stepper].stepper_movement];
-	}
+		// Setting PORTB / PORTC to the value depending on what stepper is used.
+		*stepper_motors[stepper].port = stepper_motors[stepper].stepper_values[stepper_motors[stepper].stepper_movement];
 }
 
 void button_check() {
