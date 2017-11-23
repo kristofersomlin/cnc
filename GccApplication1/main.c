d57318954b04b00b6d7343f082f24576d733889a
kristofersomlin <kristofer.somlin@gmail.com>
2017-11-23 15:55:14 +0100

    enable 360 degrees

--- GccApplication1/main.c
+++ GccApplication1/main.c
@@ -2,56 +2,99 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-struct stepper_motor {
+struct stepper_motor
+{
 	uint8_t stepper_values[8];
 	int forward;
 	int stepper_movement;
 	volatile uint8_t *port;
 } stepper_motors[2] = {
+	//{{0b00000011,0b00000110,0b00001100,0b00001001},1,0,&PORTC},
 	{{0b00000001,0b00000011,0b00000010,0b00000110,0b00000100,0b00001100,0b00001000,0b00001001},1,0,&PORTC},
 	{{0b00000001,0b00000011,0b00000010,0b00000110,0b00000100,0b00001100,0b00001000,0b00001001},1,0,&PORTB}
 };
 
 // Ändra denna funktion till en int som returnerar 1 om den funkar och 0 om det blir fel?!?!?!?!?
-void stepper_move(int stepper) {
-	if(stepper_motors[stepper].forward) {
-		if(stepper_motors[stepper].stepper_movement<7){
+void stepper_move(int stepper)
+{
+	// Checking if the motor should move forward
+	if(stepper_motors[stepper].forward)
+	{
+		if(stepper_motors[stepper].stepper_movement<7)
+		{
 			stepper_motors[stepper].stepper_movement++;
-		} else {
+		}
+		else
+		{
 			stepper_motors[stepper].stepper_movement=0;
 		}
-	} else {
-		if(stepper_motors[stepper].stepper_movement>0) {
+	}
+	else
+	{
+		if(stepper_motors[stepper].stepper_movement>0)
+		{
 			stepper_motors[stepper].stepper_movement--;
-		} else {
+		}
+
+		else
+		{
 			stepper_motors[stepper].stepper_movement=7;
 		}
 	}
 		// Setting PORTB / PORTC to the value depending on what stepper is used.
 		*stepper_motors[stepper].port = stepper_motors[stepper].stepper_values[stepper_motors[stepper].stepper_movement];
-		//PORTC = stepper_motors[stepper].stepper_values[stepper_motors[stepper].stepper_movement];
 }
-/*
-void button_check() {
-	if((PINB & (1<<PB0)) == 0) {
-		stepper_motors[0].forward=0;
-		} else {
-		stepper_motors[0].forward=1;
-	}
-}
-*/
-int main(void) {
+
+int main(void)
+{
+
 	DDRC |= 0xFF;
 	DDRB |= 0xFF;
-	//PORTB = 0x01;
-
+	int pwm_timer=0;
+	int maxmovement=0;
+	double inc_procent=0.2;
 	while (1)
 	{
-		//button_check();
+		
+ 		//stepper_move(1);
+		//_delay_us(90);
+
+
+
+
+
+
 		stepper_move(0);
-// 		_delay_us(700);
- 		stepper_move(1);
-		_delay_us(90);
-	
+		for(int i=0;i<pwm_timer;i++) {
+			_delay_us(1);
+		}
+	//	PORTC = 0b00000000;
+		for(int i=0;i<240-pwm_timer;i++) {
+			_delay_us(1);
+		}
+		
+		
+		if (maxmovement<100) {
+			inc_procent+=0.01;
+			//timershitbalaha++;
+			pwm_timer=(int)(inc_procent*232);
+		}
+
+		if(maxmovement>300) {
+			inc_procent-=0.01;
+			//timershitbalaha++;
+			pwm_timer=(int)(inc_procent*232);
+		}
+		if(maxmovement>400) {
+			PORTC = 0b00000000;
+			_delay_ms(1000);
+			maxmovement=0;
+			inc_procent=0.2;
+		}
+maxmovement++;
+		
+		//	stepper_motors[0].forward=1;
+		
+
 	}
 }
